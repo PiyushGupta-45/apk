9c7eb55ffc048ddbdd8aa1e0a327b6814b57f6ce
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2025-11-21 07:17:04 +0530

    done

---
+++ Apk/src/App.jsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Download Our Mobile App</h1>
+      <p>Click below to download the latest Android APK.</p>
+
+      <a
+        href="/app-release.apk"
+        download
+        style={{
+          padding: "12px 25px",
+          background: "#4C5BF1",
+          color: "#fff",
+          borderRadius: "8px",
+          textDecoration: "none",
+          fontSize: "20px",
+        }}
+      >
+        Download APK
+      </a>
+    </div>
+  );
+}
+
+export default App;
