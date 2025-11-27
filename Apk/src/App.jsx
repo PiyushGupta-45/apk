bb90d760a9b9fa596a26302f8a91bcecead8f0e8
Piyush Gupta <157828381+PiyushGupta-45@users.noreply.github.com>
2025-11-27 18:03:58 +0530

    commit

--- Apk/src/App.jsx
+++ Apk/src/App.jsx
@@ -1,11 +1,30 @@
 function App() {
   return (
     <div style={{ textAlign: "center", padding: "40px" }}>
-      <h1>Download Our Mobile App</h1>
+      <h1>Download Our Health Hub Mobile App</h1>
       <p>Click below to download the latest Android APK.</p>
 
       <a
         href="https://github.com/PiyushGupta-45/New_Health_hub/releases/download/1.0.2/app-release.apk"
+        download
+        style={{
+          padding: "12px 25px",
+          background: "#4C5BF1",
+          color: "#fff",
+          borderRadius: "8px",
+          textDecoration: "none",
+          fontSize: "20px",
+        }}
+      >
+        Download APK
+      </a>
+
+
+      <h1>Download Our Share Bite Mobile App</h1>
+      <p>Click below to download the latest Android APK.</p>
+
+      <a
+        href="https://github.com/PiyushGupta-45/ShareBite/releases/download/1.0.0/app-release.apk"
         download
         style={{
           padding: "12px 25px",
